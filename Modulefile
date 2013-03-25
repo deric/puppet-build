66520e5310025e381c32793b9b02744c59d6a110
Tomas Barton <barton.tomas@gmail.com>
2013-03-25 23:14:31 +0100

    implementation of build function

--- Modulefile
+++ Modulefile
@@ -3,9 +3,9 @@ version '0.0.1'
 source 'git://github.com/deric/puppet-build.git'
 author 'deric'
 license 'Apache License, Version 2.0'
-summary 'UNKNOWN'
+summary 'Function for installing from source code when no binary package available'
 description 'UNKNOWN'
-project_page 'UNKNOWN'
+project_page 'https://github.com/deric/puppet-build'
 
 ## Add dependencies, if any:
 # dependency 'username/name', '>= 1.2.0'
--- manifests/init.pp
+++ manifests/init.pp
@@ -23,19 +23,170 @@
 #
 # === Examples
 #
-#  class { build:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
+# build::install { 'top':
+#   download => 'http://www.unixtop.org/dist/top-3.7.tar.gz',
+#   creates  => '/usr/local/bin/top',
+# }
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Tomas Barton <barton.tomas@gmail.com>
 #
 # === Copyright
 #
-# Copyright 2013 Your name here, unless otherwise noted.
+# Copyright 2013 Tomas Barton
 #
-class build {
 
 
+define build::install (
+    $url, 
+    $creates, 
+    $owner               = 'root',
+    $group               = 'root',
+    $pkg_folder='', 
+    $pkg_format="tar", 
+    $pkg_extension="", 
+    $buildoptions="", 
+    $extractorcmd="",
+    $extracted_dir       = '', 
+    $rm_build_folder     = true,
+    $destination_dir     = '/tmp',
+    $work_dir            = '/tmp',
+    $path                = '/bin:/sbin:/usr/bin:/usr/sbin',
+    $extract_command     = '',
+    $preextract_command  = '',
+    $postextract_command = '',
+    $exec_env            = [],
+  ) {
+  
+  build::requires { "$name-requires-build-essential":  package => 'build-essential' }
+
+
+ #should be ok for most linux distributions
+  $tmp_dir = "/tmp"
+
+  $source_filename = url_parse($url,'filename')
+  $source_filetype = url_parse($url,'filetype')
+  $source_dirname = url_parse($url,'filedir')
+
+#  $extracted_dir = inline_template("<%= source_filename[0, source_filename.index('-')] %>")
+
+  Exec {
+    unless => "test -f $creates",
+  }
+
+
+ $real_extract_command = $extract_command ? {
+    ''      => $source_filetype ? {
+      '.tgz'     => 'tar -zxf',
+      '.tar.gz'  => 'tar -zxf',
+      '.tar.bz2' => 'tar -jxf',
+      '.tar'     => 'tar -xf',
+      '.zip'     => 'unzip',
+      default    => 'tar -zxf',
+    },
+    default => $extract_command,
+  }
+
+  $extract_command_second_arg = $real_extract_command ? {
+    /^cp.*/    => '.',
+    /^rsync.*/ => '.',
+    default    => '',
+  }
+
+  $real_extracted_dir = $extracted_dir ? {
+    ''      => $real_extract_command ? {
+      /(^cp.*|^rsync.*)/  => $source_filename,
+      default             => $source_dirname,
+    },
+    default => $extracted_dir,
+  }
+
+  if $preextract_command {
+    exec { "PreExtract ${source_filename}":
+      command     => $preextract_command,
+      before      => Exec["Extract ${source_filename}"],
+      refreshonly => true,
+      path        => $path,
+      environment => $exec_env,
+    }
+  }
+
+  exec { "Retrieve ${url}":
+    cwd         => $work_dir,
+    command     => "wget ${url} -O ${work_dir}/${source_filename}",
+    creates     => "${work_dir}/${source_filename}",
+    timeout     => 3600,
+    path        => $path,
+    environment => $exec_env,
+  }
+
+  exec { "Extract ${source_filename}":
+    command     => "mkdir -p ${destination_dir} && cd ${destination_dir} && ${real_extract_command} ${work_dir}/${source_filename} ${extract_command_second_arg}",
+    unless      => "ls ${destination_dir}/${real_extracted_dir}",
+    creates     => "${destination_dir}/${real_extracted_dir}",
+    require     => Exec["Retrieve ${url}"],
+    path        => $path,
+    environment => $exec_env,
+    notify      => Exec["Chown ${source_filename}"],
+  }
+
+  exec { "Chown ${source_filename}":
+    command     => "chown -R ${owner}:${group} ${destination_dir}/${real_extracted_dir}",
+    refreshonly => true,
+    require     => Exec["Extract ${source_filename}"],
+    path        => $path,
+    environment => $exec_env,
+    notify      => Exec["config-$name"],
+  }
+
+  if $postextract_command {
+    exec { "PostExtract ${source_filename}":
+      command     => $postextract_command,
+      cwd         => "${destination_dir}/${real_extracted_dir}",
+      subscribe   => Exec["Extract ${source_filename}"],
+      refreshonly => true,
+      timeout     => 3600,
+      require     => Exec["Retrieve ${url}"],
+      path        => $path,
+      environment => $exec_env,
+    }
+  }
+
+  $cwd    = "${destination_dir}/${real_extracted_dir}"
+
+  exec { "config-$name":
+    cwd     => "$cwd",
+    command => "$cwd/configure $buildoptions",
+    timeout => 120, # 2 minutes
+    require => Exec["Chown ${source_filename}"],
+  }
+  
+  exec { "make-install-$name":
+    cwd     => "$cwd",
+    command => "make && make install",
+    timeout => 600, # 10 minutes
+    require => Exec["config-$name"],
+  }
+  
+  # remove build folder
+  case $rm_build_folder {
+    true: {
+      notice("remove build folder")
+      exec { "remove-$name-build-folder":
+        cwd     => "$cwd",
+        command => "rm -rf $cwd",
+        require => Exec["make-install-$name"],
+      } # exec
+    } # true
+  } # case
+  
+}
+
+define build::requires ( $ensure='installed', $package ) {
+  if defined( Package[$package] ) {
+    debug("$package already installed")
+  } else {
+    package { $package: ensure => $ensure }
+  }
 }
